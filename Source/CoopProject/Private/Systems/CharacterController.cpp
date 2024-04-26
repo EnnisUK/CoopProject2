658275d5db0dc60dfbb32353dae8f9ad8c270541
Ennis <itszedam@gmail.com>
2024-04-26 22:00:12 +0100

    final build

--- Source/CoopProject/Private/Systems/CharacterController.cpp
+++ Source/CoopProject/Private/Systems/CharacterController.cpp
@@ -37,7 +37,7 @@ void ACharacterController::PushToTalk(bool TurnOnMic)
 	if (TurnOnMic)
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, TEXT("Mic On, called from CharacterCon"));
-		UUniversalVoiceChat::VoiceChatStartSpeak(true, true, 0, false);
+		UUniversalVoiceChat::VoiceChatStartSpeak(false, true, 0, false);
 	}
 	else
 	{
